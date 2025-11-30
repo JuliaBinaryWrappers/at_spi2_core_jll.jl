312fca7deee47628ee24efc2c610b7503cb19d0d
jlbuild <juliabuildbot@gmail.com>
2025-11-30 19:28:33 +0000

    at_spi2_core_jll build 2.58.2+0

--- src/at_spi2_core_jll.jl
+++ src/at_spi2_core_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("at_spi2_core")
-JLLWrappers.@generate_main_file("at_spi2_core", UUID("0fc3237b-ac94-5853-b45c-d43d59a06200"))
+JLLWrappers.@generate_main_file("at_spi2_core", Base.UUID("0fc3237b-ac94-5853-b45c-d43d59a06200"))
 end  # module at_spi2_core_jll
